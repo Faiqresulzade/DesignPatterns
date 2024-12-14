54f423dfd3edeb4918cb5d3fbdb2a370129ee85e
Faiqresulzade <faigbr@code.edu.az>
2024-12-14 13:32:17 +0400

    added singleton pattern

---
+++ SingletonDesignPattern/SingletonDesignPattern/Camera.cs
@@ -0,0 +1,38 @@
+﻿namespace SingletonDesignPattern;
+
+public class Camera
+{
+
+    //LazyLoading
+    public int Position { get; set; }
+    public string Name { get; set; }
+
+    private static volatile Camera _instance;
+
+    public static int Count = 0;
+
+    private static Object _instanceLock = new Object();
+
+    private Camera()
+    {
+        ++Count;
+        Console.WriteLine(Count);
+    }
+
+    public static Camera GetCamera()
+    {
+        //Race Condi
+        //Lock,Monitor,Mutex,Semaphore,SemaphoreSlime,ReaderWriteLock
+        lock (_instanceLock)
+        {
+            if (_instance is null)
+                _instance = new Camera();
+        }
+
+       
+        
+
+        return _instance;
+    }
+
+}
---
+++ SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using SingletonDesignPattern;
+
+//Camera camera1 = Camera.GetCamera();
+//Camera camera2 = Camera.GetCamera();
+//Camera camera3 = Camera.GetCamera();
+//Camera camera4 = Camera.GetCamera();
+//Camera camera5 = Camera.GetCamera();
+//Camera camera6 = Camera.GetCamera();
+//Camera camera7 = Camera.GetCamera();
+//io,cpu
+//CreateThread, CreateremoteThread
+
+Parallel.For(0, 10, (item) =>
+{
+    Camera camera1 = Camera.GetCamera();
+
+});
+
